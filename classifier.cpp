684f959f59f3e87512f8336b6558dd439551e5a6
ericaluksts2 <eluksts2@outlook.com>
2021-04-19 15:42:35 -0400

    Initial commit for classifier.cpp includes data, getData(),setData()

---
+++ classifier.cpp
@@ -0,0 +1,44 @@
+//	FILE			:	classifier.cpp
+//	PROJECT			:	SEF Assignment 5
+//	PROGRAMMER		:	Erica Luksts
+//	FIRST-VERSION	:	April 19, 2021
+//	DESCRIPTION		:	This program creates a class called Classifier, which includes an int called data,
+//	a function called train(), and a function called predict(). Each function prints the function name. 
+//	This program used to demonstrate commit practice using git.
+
+#include <iostream>
+
+using namespace std;
+
+class Classifier
+{
+	int data;
+
+public:
+
+	//
+	//	FUNCTION	:	getData
+	//	DESCRIPTION	:
+	//		This function provides the data value
+	//	PARAMETERS	:	NONE
+	//	RETURNS		:	
+	//		int		:	data
+	int getData()
+	{
+		return data;
+	}
+
+	//
+	//	FUNCTION	:	setData
+	//	DESCRIPTION	:
+	//		This function provides the data value
+	//	PARAMETERS	:	
+	//		int dataInput	:	integer data value
+	//	RETURNS		:	NONE
+	void setData(int dataInput)
+	{
+		data = dataInput;
+	}
+
+	//insert train() and predict() functions after
+};
